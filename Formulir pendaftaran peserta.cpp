2f1bac24592026e792bea545c305827f26525b96
CELY FEBITA ILHAM <92569794+CelyFebitaIlham@users.noreply.github.com>
2021-11-22 14:41:54 +0700

    Add files via upload

---
+++ Formulir pendaftaran peserta.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main (){
+    //input  
+    
+    string nama_pendaftar;
+    string nomor_hp_pendaftar;
+    string alamat_pendaftar;
+    string alasan_mendaftar;
+    
+    cout<<"nama pendaftar =";
+    getline (cin,nama_pendaftar);
+    cout<<"nomor hp pendaftar =";
+    getline (cin,nomor_hp_pendaftar);
+    cout<<"alamat pendaftar =";
+    getline (cin,alamat_pendaftar);
+    cout<<"alasan mendaftar =";
+    getline (cin,alasan_mendaftar);
+    
+    //output
+    cout << "nama pendaftar     = "<<nama_pendaftar<<endl;
+    cout << "nomor hp pendaftar = "<<nama_pendaftar<<endl;
+    cout << "alamat pendaftar   = "<<alamat_pendaftar<<endl;
+    cout << "alasan mendaftar   = "<<alasan_mendaftar<<endl;
+    
+    return 0;
+}
+    
+    
